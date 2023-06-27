95a5e7ed7d30a6fab5ab78dd561b9e12eaee6d67
Ruby O'Connor <rubyoconnor@Rubys-MBP-2.smc.local>
2023-06-27 12:12:37 +1200

    Delete,update and add news working

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from db_functions import run_search_query_tuples
+from db_functions import run_search_query_tuples, run_commit_query
 from datetime import datetime
 
 app = Flask(__name__)
@@ -11,7 +11,6 @@ def news_date(sqlite_dt):
     # create a date object
     x = datetime.strptime(sqlite_dt, '%Y-%m-%d %H:%M:%S')
     return x.strftime("%a %d %b %Y %H:%M %p")
-
 
 @app.route('/')
 def index():
@@ -31,7 +30,6 @@ def regions():
 
 @app.route('/news')
 def news():
-
     # query for the page
     sql = """select news.news_id, news.title, news.subtitle, news.content, news.newsdate, member.name
          from news
@@ -42,6 +40,79 @@ def news():
     result = run_search_query_tuples(sql, (), db_path, True)
     print(result)
     return render_template("news.html", news=result)
+
+@app.route('/news_formpage', methods =['GET', 'POST'])
+def news_formpage():
+    # colllect data from the web address
+    # this happens regardless of GET or POST
+    data = request.args
+    required_keys = ['id', 'task']
+    # check that we have the required keys
+    # run error page if a problem
+    for k in required_keys:
+        if k not in data.keys():
+            message = "Do not know what to do with create read update on news (key not present)"
+            return render_template('error.html', message=message)
+
+            # yes, we have an id and a task key
+            # if I have arrived directly from the page
+    if request.method == "GET":
+        # is the task to delete ?
+        if data['task'] == 'delete':
+            sql = "delete from news where news_id = ?"
+            values_tuple = (data['id'],)
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('news'))
+
+        elif data['task'] == 'update':
+            sql = """ select title, subtitle, content from news where news_id=?"""
+            values_tuple = (data['id'],)
+            result = run_search_query_tuples(sql, values_tuple, db_path, True)
+            result = result[0]
+            return render_template("news_formpage.html",
+                                    **result,
+                                    id = data['id'],
+                                    task = data['task'],)
+
+        elif data['task'] == 'add':
+            # dummy data for testing
+            temp = {'title': 'Test Title', 'subtitle': 'Test subtitle', 'content': 'Test Content'}
+            return render_template("news_formpage.html",
+                                    id=0,
+                                    task=data['task'],
+                                    title=temp['title'],
+                                    subtitle=temp['subtitle'],
+                                    content=temp['content'])
+
+        else:
+            message = "Unrecognised task coming from news page"
+            return render_template(error.html, message=message)
+
+    elif request.method == "POST":
+       # collected form information
+       f = request.form
+       print(f)
+       if data['task'] =='add':
+           # add the new news entry to the database
+           sql = """insert into news(title,subtitle,content, newsdate, member_id) 
+                                values(?,?,?, datetime('now', 'localtime'),2)"""
+           values_tuple = (f['title'], f['subtitle'], f['content'])
+           result = run_commit_query(sql, values_tuple, db_path)
+           # this will redirect to news page to see the newly added news item
+           return redirect(url_for('news'))
+
+       elif data['task'] == 'update':
+           # we are updating so 'rewrite' all the data even if
+           sql = """update news set title=?, subtitle=?, content=?, 
+                   newsdate=datetime('now', 'localtime') where news_id=?"""
+           values_tuple = (f['title'], f['subtitle'], f['content'], data['id'])
+           result = run_commit_query(sql, values_tuple, db_path)
+           # collect the data from the form and update the database at the sent id
+           return redirect(url_for('news'))
+       else:
+           # let's put in an error catch
+           message = "Unrecognised task coming from news form submission"
+           return render_template('error.html', message=message)
 
 
 if __name__ == "__main__":
